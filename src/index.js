734e38b8146c1c938f8ea94076af73d41c6de0fe
John Plummer <john@memphis.co.uk>
2017-02-05 19:25:33 +0000

    Mock the API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './index.css';
 
 import {
-  getUsers
+  getUsers,
+  deleteUser
 } from './api/userApi';
 
 // Populate table of users via API call.
@@ -19,4 +20,20 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+
+  });
+
 });
