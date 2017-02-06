2681a72bc2d048a7d431b2c64a388482b2e185e6
John Plummer <john@memphis.co.uk>
2017-02-06 17:40:27 +0000

    Fix some formatting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './index.css';
-
-import {
-  getUsers,
-  deleteUser
-} from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
